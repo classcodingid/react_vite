501c8ba2a431238246f27983cefefec0b545eacd
classcodingid <saefulmujab.restuikami@gmail.com>
2023-10-01 11:52:00 +0700

    fitur login register

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import ProjectList from './pages/ProjectList'
 import ProjectCreate from './pages/ProjectCreate'
 import ProjectEdit from './pages/ProjectEdit'
 import ProjectShow from './pages/ProjectShow'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path='/' element={<ProjectList />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/project' element={<ProjectList />} />
         <Route path='/create' element={<ProjectCreate />} />
         <Route path='/edit/:id' element={<ProjectEdit />} />
         <Route path='/show/:id' element={<ProjectShow />} />
